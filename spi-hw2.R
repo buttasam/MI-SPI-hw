a423b07bd4348afe4b5f0f4c664cc50eda316d49
Vojtech Mraz <mraz.vojta@gmail.com>
2018-05-14 11:54:18 -0700

    HW2 DONE

--- spi-hw2.R
+++ spi-hw2.R
@@ -5,18 +5,30 @@ L = 5
 X = ((K * L * 23) %% (20)) + 1
 # X = 6 
 
-# prvni soubor
 file <- readLines("/home/zeel/Documents/SPI/hw1_source/006.txt")
 
-# 1 
+### 1 
 chars <- strsplit(gsub(" ", "_", file[2] , fixed = TRUE), "")
 t <- table(chars)
-p <- prop.table(t)
-plot(p)
+p1 <- prop.table(t)
+barplot(p1)
 
-# 2
+### 2
 tm <- createSequenceMatrix(chars, toRowProbs = TRUE, sanitize = FALSE, possibleStates = character())
 View(tm)
 
-# 3
+### 3
+col_num <- 27
+# vytvoření početní matice
+Ab <- t(diag(rep(1, col_num)) - tm)
+# prave strany
+Bb <- rep(0, col_num)
+# pridat omezeni, vse se rovna 1
+Ac <- rbind(Ab, rep(1, col_num))
+Bc <- c(Bb, 1)
+# solve matice
+p2 <- t(solve(t(Ac)%*%Ac, t(Ac)%*%Bc))
+barplot(p2%*%tm)
 
+### 4
+t.test(p1, p2)
